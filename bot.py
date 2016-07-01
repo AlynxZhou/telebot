58ac89bd8e415c9f183fa30254d59841416400c1
S-X-ShaX <sxshax@gmail.com>
2016-07-01 14:22:07 +0800

    A commit.

--- bot.py
+++ bot.py
@@ -95,6 +95,15 @@ def joke():
     return joke_list
 joke_list = joke()
 
+fuck_list = [
+    "NO!'m not a GAY!",
+    "Fuck you!",
+    "So painful!",
+    "Oh!NO!",
+    "Ya Mie Die!",
+    "Ah,so comfortable.",
+    "Good boy,since then I am your girl."
+]
 
 #class MessageCounter(telepot.helper.ChatHandler):
 #    def __init__(self, seed_tuple, timeout):
@@ -149,10 +158,11 @@ class TeleBot(telepot.helper.ChatHandler):
             elif self._text == "/time" or (self._text == "/time@" + info["username"]):
                 self._answer = "Now is " + str(datetime.datetime.now()) + "."
             elif self._text == "/fuck" or (self._text == "/fuck@" + info["username"]):
-                self._answer = "NO!'m not a GAY!"
+#                self._answer = "NO!'m not a GAY!"
+                self._answer = fuck_list[self._fuck]
                 self._fuck += 1
-            elif (self._text == "/fuckagain" or (self._text == "/fuckagain@" + info["username"])) and self._fuck >= 1:
-                self._answer = "Fuck you!"
+#            elif (self._text == "/fuckagain" or (self._text == "/fuckagain@" + info["username"])) and self._fuck >= 1:
+#                self._answer = "Fuck you!"
             elif self._text == "/count" or (self._text == "/count@" + info["username"]):
                 self._answer = self._count
             elif self._text_1 == "/cmd" or (self._text_1 == "/cmd@" + info["username"]):
