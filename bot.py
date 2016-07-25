bf8c3041214884637cb9941e9f51c275df8f1dfc
S-X-ShaX <sxshax@gmail.com>
2016-07-25 11:38:29 +0800

    A commit.

--- bot.py
+++ bot.py
@@ -394,7 +394,7 @@ class TeleBot(telepot.helper.UserHandler):
 
         ## Journal.
         if self._answer != None:
-            print("\033[33m>>>\033[0m %s\n\033[33mBot\033[0m: Close an delegator with @\033[34m%s\033[0m by calling on_close()."%(self._now, self._username))
+            print("\033[33m>>>\033[0m %s\n\033[33mBot\033[0m: Closed an delegator with @\033[34m%s\033[0m by calling on_close()."%(self._now, self._username))
             print("--------------------------------------------")
 
 
