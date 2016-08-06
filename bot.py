a86896811eb6e07e2ea1d349aa86740390a28eb0
S-X-ShaX <sxshax@gmail.com>
2016-08-06 20:58:31 +0800

    A bug can't be fix...

--- bot.py
+++ bot.py
@@ -339,7 +339,7 @@ class TeleBot(telepot.helper.UserHandler):
                                 if not rule_dict[key] in self._answers:
                                     self._answers += rule_dict[key] + '\n'
                                     self._answer = self._answers.rstrip('\n')
-                elif random.random() <= 0.5:
+                elif random.random() <= 0.3:
                     self._answer = random.choice(list(resource.sticker_dict.keys()))
                     self._sticker = resource.sticker_dict[self._answer]
 
